7d9c23e48fb5b194513722985a7836c02899954a
Bruno Rodrigues de Carvalho <rodrigues_b@hotmail.com>
2022-04-15 20:47:12 -0300

    merge CRUD categorias e usuários

---
+++ src/controller/questionControllerV3.js
@@ -0,0 +1,259 @@
+const { Router } = require('express');
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId
+const uri = process.env.mongoDbURI
+const { route } = require('express/lib/application');
+const { ObjectID } = require('mongodb');
+const { isValidObjectId } = require('../database');
+const authMiddleware = require('../middlewares/auth')
+const bancodedados = 'QuestDB'
+const colecao = 'QuestQuestionv2'
+
+const router = express.Router()
+
+router.use(authMiddleware)
+
+//Rota após o login - Desvio Super Admin
+router.get('/', async(req,res) => {
+    emailJogador = req.session.email
+    //console.log("emailJogador: ", emailJogador)
+    auth = req.headers.authorization || req.body.authorization || req.session.authorization
+    nomeJogador = req.session.username
+    //console.log("Token recebido no Servidor - question Controler 1: ", auth)
+    console.log("Quem tentou jogar - question Controler 1: ", nomeJogador)
+
+    if (emailJogador == "superadmin@superadmin") {
+        res.render('admin', {nomeJogador: nomeJogador, perfil: "SUPER ADMIN"})
+    } else {
+        res.render('jogo', {nomeJogador: nomeJogador, perfil: "Jogador"})
+    }
+})
+
+//CREATE
+router.post('/pergunta', async(req, res) => {  
+    if(!req.body.categoria) {
+        res.status(461).send("Falha do Cliente: Faltou informar a categoria")
+        // Falta testar se é uma categoria válida!
+    } else if (!req.body.pergunta) {
+        res.status(462).send("Falha do Cliente: Faltou informar a pergunta")
+    } else if (!req.body.respostaCorreta) {
+        res.status(463).send("Falha do Cliente: Faltou informar a resposta correta")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaA) {
+        res.status(464).send("Falha do Cliente: Faltou informar alternativaA")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaB) {
+        res.status(465).send("Falha do Cliente: Faltou informar alternativaB")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaC) {
+        res.status(466).send("Falha do Cliente: Faltou informar alternativaC")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaA) {
+        res.status(467).send("Falha do Cliente: Faltou informar alternativaD")
+        //preciso testar algum formato?
+    } else if (!req.body.info) {
+        res.status(468).send("Falha do Cliente: Faltou informar a fonte da pergunta.")
+    } else {
+        novaPergunta = {
+            categoria : req.body.categoria,
+            pergunta : req.body.pergunta,
+            respostaCorreta : req.body.respostaCorreta,
+            alternativaA : req.body.alternativaA,
+            alternativaB : req.body.alternativaB,
+            alternativaC : req.body.alternativaC,
+            alternativaD : req.body.alternativaD,
+            info: req.body.info
+        } 
+    
+        MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, QuestDB) {
+            if (err) {
+                res.status(469).send("Erro de conexão no Mongo - Verifique a URI apontada!")
+            } else {
+                var dbo = QuestDB.db(bancodedados);
+                dbo.collection(colecao).insertOne(novaPergunta, function (err, confirmacao) {
+                    if (err) {
+                        console.log("Erro ao tentar cadastrar nova pergunta!")
+                        res.status(470).send("Impossível registrar essa pergunta. Tente outro ID ou fale com o Bruno!")
+                    } else if (!confirmacao.insertedId) {
+                        console.log("Erro ao tentar cadastrar nova pergunta!")
+                        res.status(471).send("Impossível registrar essa pergunta. ID inválido! Tente outro ID. Use sempre números!")
+                    } else {
+                        console.log("Pergunta cadastrada: ", confirmacao.insertedId)
+                        res.send("Pergunta cadastrada com sucesso!")
+                    }
+                    QuestDB.close(); 
+                })}
+        })
+    }
+})
+
+//READ
+
+//READ - SIMPLES - TODAS AS PERGUNTAS
+router.get('/perguntas', async(req,res)=>{   
+    MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, QuestDB) {
+        if (err) throw err;
+        var dbo = QuestDB.db(bancodedados);
+        dbo.collection(colecao).find().toArray(function await(err, questions) {
+            if (err) throw err;
+            res.status(200).send(questions)
+            QuestDB.close(); 
+        })
+    })
+})
+
+//READ - REGRA DE FRONT - ÚNICA PERGUNTA QUALQUER DESDE QUE AINDA NÃO RESPONDIDA DURANTE A SESSÃO
+//*** PARA SABER AS PERGUNTAS JÁ RESPONDIDAS, O FRONT PRECISA INFORMAR O ID DA PERGUNTA
+router.get('/pergunta', async(req,res) => {
+    if(!req.body.categoria || !req.body) {
+        res.status(400).send("Tá de zoeira né tio?! rsrs")
+    } else {
+        var categoriaReq = String(req.body.categoria)
+        var respondidas = req.body.questoes_ja_respondidas
+
+        //console.log("Categorias Informadas - REQ: ", categoriaReq)
+        //console.log("Questões Respondidas - REQ: ", respondidas)
+        ///console.log(respondidas.length)
+
+        var filtro
+
+        if(respondidas.length==0 && !req.body.categoria) {
+            //console.log("Sem info de ID")
+            filtro = {}
+        } else {
+            filtro = []
+            filtro.push({ categoria: categoriaReq })
+
+            if (respondidas.length===0) {
+
+                //console.log("Sem perguntas respondidas!")
+            
+                filtro = { categoria: categoriaReq }
+
+            } else {
+
+                for(i=0;i<respondidas.length;i++){
+                    item = respondidas[i]
+                    item = item._id
+                    item = ObjectId(item)
+                    filtro.push( { _id : { $ne: item } } )
+                }
+
+                filtro = { $and: filtro }
+
+                //console.log("filtro2: ", filtro)
+            
+            }
+        }
+
+        MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, QuestDB) {
+            if(err) throw err;
+            var dbo = QuestDB.db(bancodedados);
+            dbo.collection(colecao).find(filtro).limit(1).toArray(function await(err, questions) {
+                if(err) throw err
+                else if(questions.length==0) {
+                    res.status(400).send("Deu ruim, pois a query não tem nenhum resultado com esses filtros (Categoria + Exclusão de certos IDs")
+                } else {
+                    res.status(200).send(questions)
+                }
+                QuestDB.close(); 
+            })
+        })
+    }
+})
+
+//UPDATE
+router.put('/pergunta', async(req, res) => {
+    
+    if(!req.body._id) {
+        res.status(401).send("Falha do Cliente: Faltou informar ID da pergunta")
+    } else if(!req.body.categoria) {
+        res.status(401).send("Falha do Cliente: Faltou informar a categoria")
+        // Falta testar se é uma categoria válida!
+    } else if (!req.body.pergunta) {
+        res.status(401).send("Falha do Cliente: Faltou informar a pergunta")
+    } else if (!req.body.respostaCorreta) {
+        res.status(401).send("Falha do Cliente: Faltou informar a resposta correta")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaA) {
+        res.status(401).send("Falha do Cliente: Faltou informar alternativaA")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaB) {
+        res.status(401).send("Falha do Cliente: Faltou informar alternativaB")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaC) {
+        res.status(401).send("Falha do Cliente: Faltou informar alternativaC")
+        //preciso testar algum formato?
+    } else if (!req.body.alternativaA) {
+        res.status(401).send("Falha do Cliente: Faltou informar alternativaD")
+        //preciso testar algum formato?
+    } else if (!req.body.info) {
+        res.status(401).send("Falha do Cliente: Faltou informar a fonte")
+    } else {
+        console.log("_id:", req.body._id)
+
+        MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, QuestDB) {
+            if (err) { 
+                res.status(400).send("Erro na conexão") 
+            }
+            else {
+                var dbo = QuestDB.db(bancodedados);
+
+                var pergunta = { 
+                    $set: {
+                        categoria : req.body.categoria,
+                        pergunta : req.body.pergunta,
+                        respostaCorreta : req.body.respostaCorreta,
+                        alternativaA : req.body.alternativaA,
+                        alternativaB : req.body.alternativaB,
+                        alternativaC : req.body.alternativaC,
+                        alternativaD : req.body.alternativaD,
+                        info: req.body.info
+                    }
+                }
+
+                dbo.collection(colecao).updateOne({"_id": ObjectId(req.body._id)}, pergunta, function (err, confirmacao) {
+                    if (err) {
+                        console.log("Erro ao tentar alterar pergunta: ", err)
+                        res.status(400).send("Impossível alterar essa pergunta. Tente outro ID ou insulte o Bruno!")
+                    } else {
+                        console.log("Pergunta alterada com sucesso: ",confirmacao)
+                        res.status(200).send("Pergunta alterada com sucesso.")
+                    }
+                    QuestDB.close(); 
+                })
+            }
+        })
+    }
+})
+
+
+//DELETE
+router.delete('/pergunta', async(req, res) => {
+
+    console.log("_id:", req.body._id)
+
+    MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, QuestDB) {
+        if (err) { 
+            res.status(461).send("Erro na conexão") 
+        }
+        else {
+            var dbo = QuestDB.db(bancodedados);
+            dbo.collection(colecao).deleteOne({"_id": ObjectId(req.body._id)}, function (err, confirmacao) {
+                if (err) {
+                    console.log("Erro ao tentar deletar pergunta: ", err)
+                    res.status(462).send("Impossível deletar essa pergunta. Tente outro ID ou insulte o Bruno!")
+                } else if (confirmacao.deletedCount == 1) {
+                    console.log("Pergunta deletada com sucesso: ",confirmacao)
+                    res.status(200).send("Pergunta deletada com sucesso.")
+                } else {
+                    res.status(463).send("Deu algum outro erro! Insulte o Bruno!")
+                }
+                QuestDB.close(); 
+            })
+        }
+    })
+})
+
+module.exports = app => app.use('/jogoV3', router)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 //Importar biblioteca Express
 const express = require('express')
 const bodyParser = require('body-parser')
+const sessions = require('express-session');
+var session
 
 //Para usar bibilioteca Express
 const app = express()
@@ -9,7 +11,7 @@ const app = express()
 app.use(bodyParser.json())
 
 //Para o servidor entender/decodar quando parâmetros forem passados pela URL
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded( {extended: false} ))
 
 //Cookie-Parser
 const cookieParser = require('cookie-parser')
@@ -23,7 +25,7 @@ const path = require('path');
 //ObjectID ajuda no controle das chaves de identificação
 const { ObjectId } = require("mongodb");
 
-const { stringify } = require("querystring");
+const { stringify } = require("querystring");0
 
 //CORS
 const cors = require('cors')
@@ -32,15 +34,23 @@ const cors = require('cors')
 const PORT = process.env.PORT || 4201
 const msg_PORT = `Servidor Node.JS para QUEST FATEC disponível via porta ${PORT}!`
 
-//hard coded configuration object
-const confCors = {
+app.use(sessions({
+    secret: "perauvamacaosaladaMista*$¨#$@$#@%@$#&*#¨%¨#thisismysecrctekabobraseyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false
+}));
 
+// hard coded configuration object
+const confCors = {
+ 
     // origin undefined handler
     // see https://github.com/expressjs/cors/issues/71
     originUndefined: function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Credentials', true);
         next()
     },
-
+ 
     // Cross Origin Resource Sharing Options
     cors: {
         // origin handler
@@ -54,11 +64,9 @@ const confCors = {
 }
 
 app.use(confCors.originUndefined , cors(confCors.cors))
-
-
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false}))
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger/novoswagger.json');
@@ -66,15 +74,31 @@ const swaggerDocument = require('../swagger/novoswagger.json');
 //app.use('/swagger', express.static('swagger'));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Define a View Engine como EJS
+app.set('view engine', 'ejs');
+app.set('views', './views');
+//console.log("EJS engine view: OK.")
+
+// Definição das variáveis para criar a DOM para o JQuery funcionar com o EJS
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
 
 require('./controller/authController')(app)
-require('./controller/questionController')(app)
-require('./controller/questionControllerV2')(app)
+require('./controller/categoryController')(app)
+require('./controller/questionControllerV3')(app)
 
+//Criar a rota principal
 app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('Bem Vindo ao Quest - Cadastre-se ou Faca Login para jogar...')
-})
+    res.render('login');
+});
+
+app.get('/auth/new', (req, res) => {
+    res.render('novologin');
+});
 
 app.listen(PORT, () => {
     console.log(`${msg_PORT}`)
