269c0ba556cc3258b6e47996ecd2ea4947251871
hendyy <hendyy@gmail.com>
2022-05-09 13:56:51 -0300

    Pagina do jogo
    Inclusao da pagina do jogo e funcionalidades no server.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 //Importar biblioteca Express
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const sessions = require('express-session');
+
 var session
 
 //Para usar bibilioteca Express
 const app = express()
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 //Para entender arquivos JSON  
 app.use(bodyParser.json())
@@ -26,8 +30,6 @@ const path = require('path');
 const { ObjectId } = require("mongodb");
 
 const { stringify } = require("querystring"); 0
-
-
 
 //CORS
 const cors = require('cors')
@@ -107,6 +109,84 @@ app.get('/auth/new', (req, res) => {
     res.render('novologin');
 });
 
-app.listen(PORT, () => {
+//app.listen(PORT, () => {
+//    console.log(`${msg_PORT}`)
+//});
+http.listen(PORT, () => {
     console.log(`${msg_PORT}`)
 });
+
+const gameTable = {
+    casas:
+      ['início',
+        'M', 'S', 'E', 'CT',
+        'AE', 'V', 'E', 'S',
+        'M', 'CT', 'S', 'V',
+        'M', 'AE', 'E', 'M',
+        'AE', 'V', 'S', 'CT',
+        'E', 'M', 'AE', 'V',
+        'M', 'S', 'E', 'CT',
+        'fim'],
+    cores: {'início': '#19af54',
+          'M': '#fd8f36',
+          'S': '#a745fb',
+          'E': '#8afd40',
+          'CT': '#31a6fc',
+          'AE': '#fc2c32',
+          'V': '#fcfd45',
+          'fim': '#19af54'}
+}
+
+var numberPlayers = 0;
+var socketRed;
+var socketBlue;
+var gameState = {
+    posicaoRed: 0,
+    posicaoBlue: 0,
+    fichasRed: [true, true, true, true, true],
+    fichasblue: [true, true, true, true, true]
+};
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+    io.emit('gameTable', gameTable);
+    io.emit('gameState', gameState);
+    console.log('new connection', socket.id);
+    msgRetorno = startPlayer( socket.id);
+    io.emit('chat message', 'new player ' + numberPlayers + ' ' + msgRetorno);
+    console.log('new player ' + numberPlayers);
+    socket.on('disconnect', () => {
+        
+        if (socket.id=socketRed) {
+            console.log('player red disconnected');
+        } else if (socket.id=socketBlue) {
+            console.log('player blue disconnected');
+        } else {
+            console.log('user ' + socket.id + ' disconnected');
+        }
+        numberPlayers -= 1;
+    })
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    })
+
+    socket.on('moveRed', (casa) => {
+        io.emit('moveRed', casa);
+    })
+})
+
+function startPlayer(socketId) {
+    numberPlayers += 1;
+    if (numberPlayers==1){
+        socketRed=socketId;
+        return 'Jogador Vermelho';
+    } else if (numberPlayers==2){
+        socketBlue=socketId;
+        return 'Jogador Azul';
+    } else {
+        return 'Observador';
+    }
+}
