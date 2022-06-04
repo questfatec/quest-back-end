0ff502b5d9a60a90a1c98518a91776a136ecebe4
Bruno Rodrigues de Carvalho <rodrigues_b@hotmail.com>
2022-06-04 17:37:58 -0300

    Quase finalizando sistema de pagamento

--- public/src/comprar.js
+++ public/src/comprar.js
@@ -1,63 +1,121 @@
-function comprar() {
+let agora =  new Date()
+let anoHj = agora.getFullYear()
+let mesHj = agora.getMonth()
 
-    let agora =  new Date()
-    let anoHj = agora.getFullYear()
-    let mesHj = agora.getMonth()
+tipo = sessionStorage.getItem('vip');
 
-    var regcompra = {
-        idUser : sessionStorage.getItem('_id'),
-        dataUniversalCompra : agora.toISOString(),
-        ncc : document.getElementById("ncc").value,
-        nccExpMes : document.getElementById("nccExpMes").value,
-        nccExpAno : document.getElementById("nccExpAno").value,
-        nccCvc : document.getElementById("nccCvc").value,
-        nccNome : document.getElementById("nccNome").value,
-        nccCPF : document.getElementById("nccCPF").value
-    }
+if(tipo == "true") {
+    tipo = "Cancelamento"
+    console.log("tip txs:", tipo)
+} else if(tipo == "false") {
+    tipo = "Compra"
+    console.log("tip txs:", tipo)
+} else { console.log("Tipo de Transação não identificado")}
 
-    function validar() {
-        //Testar se número do cartão foi preenchido
-        if (
-                regcompra.ncc == "" || 
-                regcompra.ncc.length < 15 ||
-                regcompra.ncc < 3399999999999999 ||
-                regcompra.ncc > 6599999999999999 
-            ) {
-            
-                alert("Por favor informe um número de cartão de crédito válido!")
+let reg = {
+    idUser : "",
+    dataUniversalCompra : "",
+    ncc : "",
+    nccExpMes : "",
+    nccExpAno : "",
+    nccCvc : "",
+    nccNome : "",
+    nccCPF : "",
+    tipoTsx : tipo
+}
 
-        } else if (regcompra.nccNome == "") {
 
-            alert("Por favor informe o nome como está no cartão de crédito!")
-
-        } else if (regcompra.nccExpAno <= anoHj && regcompra.nccExpMes <= mesHj +1  ) {
-
-            alert("Parece que o cartão de crédito já venceu ou irá expirar neste mês. Por favor verifique o ano informado e tente novamente.")
-
-        } else if (
-                regcompra.nccCvc == "" ||
-                regcompra.nccCvc.length < 1 || 
-                regcompra.nccCvc < 1
-            ) {
-
-            alert("Por favor informe o código de segurança do cartão de crédito!")
-
-        } else if (
-                regcompra.nccCPF == "" ||
-                regcompra.nccCPF.length < 11 || 
-                regcompra.nccCPF < 1000000
-            ) {
-
-            alert("Por favor informe o CPF associado ao cartão de crédito!")
-
-        } else {
-
-            alert("Agora só enviar e gravar no bd o registro e alterar o status do usuário para VIP!")
-            console.log(regcompra)
+function alterarPerfil(reg) {
+    $.ajax({
+        url: "/pagamento/gerenciar",
+        type: 'PUT',
+        data: reg,
+        success: function(resposta) {
+            if(reg.tipoTsx == "Cancelamento") {
+                window.sessionStorage.setItem('vip', false)
+            } else if (reg.tipoTsx == "Compra") {
+                window.sessionStorage.setItem('vip', true)
+            } else {
+                alert("ERRO na hora de alterar o valor da sessio de VIP!")
+            }
+        },
+        error: function(xhr) {
+            alert(xhr.status, ":", xhr.responseText)
+            console.log("Não deu certo a alteração!!: " + xhr.status + " " + xhr.responseText);
         }
+    });
+}
+
+function validar(reg) {
+    //Testar se número do cartão foi preenchido
+    if (
+            reg.ncc == "" || 
+            reg.ncc.length < 15 ||
+            reg.ncc < 3399999999999999 ||
+            reg.ncc > 6599999999999999 
+        ) {
+        
+            alert("Por favor informe um número de cartão de crédito válido!")
+
+    } else if (reg.nccNome == "") {
+
+        alert("Por favor informe o nome como está no cartão de crédito!")
+
+    } else if (reg.nccExpAno <= anoHj && reg.nccExpMes <= mesHj +1  ) {
+
+        alert("Parece que o cartão de crédito já venceu ou irá expirar neste mês. Por favor verifique o ano informado e tente novamente.")
+
+    } else if (
+            reg.nccCvc == "" ||
+            reg.nccCvc.length < 2 || 
+            reg.nccCvc < 1
+        ) {
+
+        alert("Por favor informe o código de segurança do cartão de crédito!")
+
+    } else if (
+            reg.nccCPF == "" ||
+            reg.nccCPF.length < 11 || 
+            reg.nccCPF < 1000000
+        ) {
+
+        alert("Por favor informe o CPF associado ao cartão de crédito!")
+
+    } else {
+        alterarPerfil(reg)
+    }
+}
+
+
+function ler(reg) {
+    console.log("reg.tipoTsx ao ler", reg)
+
+    reg.idUser = sessionStorage.getItem('_id')
+    reg.dataUniversalCompra = agora.toISOString()
+
+     if (reg.tipoTsx == "Compra") {
+        reg.ncc = document.getElementById("ncc").value,
+        reg.nccExpMes = document.getElementById("nccExpMes").value,
+        reg.nccExpAno = document.getElementById("nccExpAno").value,
+        reg.nccCvc = document.getElementById("nccCvc").value,
+        reg.nccNome = document.getElementById("nccNome").value,
+        reg.nccCPF = document.getElementById("nccCPF").value
     }
 
-    validar()
-    //console.log(req.session)
+    console.log("reg:", reg)
+}
 
+function gerenciar() {
+
+    console.log(reg.tipoTsx)
+    ler(reg)
+
+    if(reg.tipoTsx == 'Compra') {
+        validar(reg)
+    } else if (reg.tipoTsx == 'Cancelamento') {
+        console.log(reg)
+        alterarPerfil(reg)
+    } else {
+        console.log("Falha ao tentar gerenciar - comprar.js linha 113")
+    }
 }
--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -1,49 +1,70 @@
-const User = require('../models/user')
 const express = require('express')
-
-
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId
-const uri = process.env.mongoDbURI
-const authMiddleware = require('../middlewares/auth')
-
-const bancodedados = 'QuestDB'
-const colecao = 'QuestQuestionv2'
-const colecaoCategoria = 'categories'
-
 const router = express.Router()
+const authMiddleware = require('../middlewares/auth')
+const User = require('../models/user')
+
 router.use(authMiddleware)
 
-//Rota para ver situação atual VIP, comprar ou cancelar.
+//Rota para comprar ou ver situação atual VIP e cancelar.
 router.get('/', async(req,res) => {
     res.render('vip', {nomeJogador: req.session.username, vip: req.session.vip})
 })
 
+
 //Comprar Passe VIP - Registro na tabela de transações VIP e alterar no perfil do usuário
-router.put('/comprar', async (req, res) => {
-    const { regCompra } = await req.body
+router.put('/gerenciar', async (req, res) => {
+    const tsx = await req.body
+
+    console.log("Tentativa de compra: ", tsx)
 
     try{
 
-        if (!reqCompra) {
+        if (!tsx) {
             
-            res.status(401).send("Faltou enviar os dados do registro")
+            res.status(401).send("Faltou enviar os dados do registro")            
 
-        } else if ( !await User.findOne({ _id: req.body.idUser }) )
-           
+        } else if ( !await User.findOne({ _id: tsx.idUser }) ) {
+
             return res.status(412).send({error: 'Falha - Email não cadastrado!'})
 
-        var updateuser = {
-            vip: true,
-            $inc: { __v: 1}
-        }
+        } else {
 
-         await User.findOneAndUpdate({_id: req.body.idUser}, updateuser).select('+vip')
+            let vip = tsx.tipoTsx
 
-        return res.status(200).send("Usuário atualizado com sucesso - agora é VIP!" )
+            if (vip == "Compra") {
+                vip = true
+            } else if (vip == "Cancelamento") {
+                vip = false
+            }
+
+            var updateuser = {
+                VIP: vip,
+                $inc: { __v: 1}
+            }
+    
+            try {
+                
+                await User.findOneAndUpdate({_id: tsx.idUser}, updateuser).select('+VIP')
+                
+            } catch (err) {
+                
+                console.log("Erro ao tentar atualizar o status do jogador")
+
+            } finally {
+
+                console.log('Alteração do usuário realizada com sucesso.')
+
+                res.render('prelobby', {nomeJogador: nomeJogador, vip: vip, perfil: "Jogador"})
             
+            }
+               
+        }
+           
+ 
     } catch (err) {
+
         return res.status(400).send( {error: 'Falha - Não foi possível comprar o passe VIP.'} )
+
     }
 })
 
